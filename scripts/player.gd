805151f3faa5f19e15e445cc8548364d445150ab
Brett Haines <bhaines3@gatech.edu>
2020-03-16 12:21:31 -0400

    Allowing player rotation while still

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,11 +13,12 @@ func _physics_process(delta):
 		tween.interpolate_property(self, "velocity", curr_vel, 0.0, 0.6)
 		tween.start()
 	
+	if(Input.is_action_pressed("move_left")):
+		rotation -= delta * ROT_PER_SEC + delta * velocity * ROT_PER_SEC
+	if(Input.is_action_pressed("move_right")):
+		rotation += delta * ROT_PER_SEC + delta * velocity * ROT_PER_SEC
+	
 	if(is_sail_up):
-		if(Input.is_action_pressed("move_left")):
-			rotation -= delta * ROT_PER_SEC * velocity
-		if(Input.is_action_pressed("move_right")):
-			rotation += delta * ROT_PER_SEC * velocity
 		move_and_collide(Vector2.UP.rotated(rotation) * WIND_SPEED * velocity, false)
 	
 	if(Input.is_action_just_pressed("cannon_port") and is_cannon_ready_port):
