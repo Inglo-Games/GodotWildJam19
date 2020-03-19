30dd9820ed7087e2b2db9c9b1e5956ae8d1b640a
Brett Haines <bhaines3@gatech.edu>
2020-03-19 10:51:07 -0400

    Adding custom settings for subtitles and voices

--- scripts/options.gd
+++ scripts/options.gd
@@ -1,8 +1,5 @@
 extends PopupPanel
 
-var subtitles := true
-var voices := true
-
 func _ready():
 	$vbox/sliders_container/bgm_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BGM"))
 	$vbox/sliders_container/sfx_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX"))
@@ -14,10 +11,10 @@ func _on_sfx_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), value)
 
 func _on_subs_btn_toggled(button_pressed):
-	subtitles = button_pressed
+	ProjectSettings.set_setting("Accessibility/subtitles", button_pressed)
 
 func _on_voice_btn_toggled(button_pressed):
-	voices = button_pressed
+	ProjectSettings.set_setting("Accessibility/voices", button_pressed)
 
 func _on_back_btn_button_up():
 	queue_free()
