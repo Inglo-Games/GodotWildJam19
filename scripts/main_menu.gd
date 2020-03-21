7382fb21c79602f1c56d37d248e6c5e503bfd18f
Brett Haines <bhaines3@gatech.edu>
2020-03-21 11:35:01 -0400

    Adding credits popup

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var OptionsMenu = preload("res://scenes/options.tscn")
+onready var CreditsMenu = preload("res://scenes/credits.tscn")
 
 func _ready():
 	$panel/container/start_btn.grab_focus()
@@ -12,6 +13,11 @@ func _on_options_btn_button_up():
 	var opts = OptionsMenu.instance()
 	add_child(opts)
 	opts.popup_centered()
+
+func _on_credits_btn_button_up():
+	var credits = CreditsMenu.instance()
+	add_child(credits)
+	credits.popup_centered()
 
 func _on_quit_btn_button_up():
 	get_tree().quit()
