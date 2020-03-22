6aafe0c041bac46e3bab315af9f43b744e1561d1
Brett Haines <bhaines3@gatech.edu>
2020-03-21 22:21:54 -0400

    Fixing island body check

--- scripts/island.gd
+++ scripts/island.gd
@@ -11,5 +11,6 @@ func _ready():
 		area.connect("body_entered", self, "_on_body_entered")
 
 func _on_body_entered(body):
-	if body is Player:
+	# Can't use `if body is Player` because of some cyclic dependency
+	if(body.name == "player"):
 		emit_signal("line_triggered", subtitle_line)
